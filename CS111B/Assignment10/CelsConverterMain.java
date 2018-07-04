c604f29fd24df1b16233fa83b7bb88d194553d6e
LishanC <37274488+LishanC@users.noreply.github.com>
2018-07-04 10:10:08 -0700

    Create CelsConverterMain.java

---
+++ CS111B/Assignment10/CelsConverterMain.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+//create an instance of the CelsConverter class, causing to display its window
+
+public class CelsConverterMain
+{
+	 public static void main(String[] args) 
+	    {
+		JFrame frame = new CelsConverter();
+
+		frame.setVisible(true);
+	    }
+}
